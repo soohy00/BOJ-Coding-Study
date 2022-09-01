8a6f08082b2c46a62a8e765e727bcb0e828d44da
JJeJJe0113 <100130261+soohy00@users.noreply.github.com>
2022-09-01 17:56:12 +0900

    [Bronze I] Title: 일곱 난쟁이, Time: 68 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Bronze/2309. 일곱 난쟁이/일곱 난쟁이.py
@@ -0,0 +1,22 @@
+import sys
+
+acc = []
+a, b = 0, 0
+
+for i in range(9):  # 아홉 난쟁이의 키를 alist라는 리스트에 받아온다
+    acc.append(int(sys.stdin.readline()))
+
+s = sum(acc) - 100
+
+for j in range(9):   # 가짜 두 난쟁이의 키의 합이 40이 되는 경우를 찾아 리스트에서 제거한다.
+    for k in range(j+1, 9):
+        if acc[j] + acc[k] == s:
+            a = acc[j]
+            b = acc[k]
+
+acc.remove(a)
+acc.remove(b)
+acc.sort()
+
+for v in acc:   # 일곱 진짜 난쟁이들을 키 오름차순으로 출력한다.
+    print(v)
