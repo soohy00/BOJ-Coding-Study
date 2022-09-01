f36b1bf2f2471270179f2b6c49f7e15acaa75dfc
JJeJJe0113 <100130261+soohy00@users.noreply.github.com>
2022-09-01 12:57:51 +0900

    Update 한수.py
    다시 풀었는데 먼저 푼 코드만 가져가기에 코드를 수정하고 다시 올립니다.

--- 백준/Silver/1065. 한수/한수.py
+++ 백준/Silver/1065. 한수/한수.py
@@ -1,10 +1,9 @@
-num = int(input())
+n = int(input())    # 수의 범위를 받아옴
+a = 0   # 한수의 갯수를 세줄 변수
 
-hansu = 0
-for i in range(1, num+1):
-    num_list = list(map(int, str(i)))
-    if i < 100:
-        hansu += 1  # 100보다 작으면 모두 한수
-    elif num_list[0]-num_list[1] == num_list[1]-num_list[2]:
-        hansu += 1  # x의 각 자리가 등차수열이면 한수
-print(hansu)
+for i in range(1, n+1):     # 1부터 n까지 중에서 한수의 갯수를 구함
+    if i<100:
+        a += 1
+    elif( (i//100-(i%100//10)) == ((i%100//10)-i%10) ):    # 100이 넘는수는 백의자리 수와 십의 자리 수의 차이가 십의 자리 수와 일의 자리 수의 차이가 같은지 확인하고 같으면 한수로 취급한다.
+        a += 1
+print(a)
