f2b56a7b37ed69232b653f1e242ac33cc9bd9771
JJeJJe0113 <100130261+soohy00@users.noreply.github.com>
2022-09-02 00:35:25 +0900

    Update 영화감독 숌.py
    delate the old code and plus new code

--- 백준/Silver/1436. 영화감독 숌/영화감독 숌.py
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.py
@@ -1,10 +1,13 @@
-n = int(input())
-cnt = 0
-six_n = 666
+import sys
+
+Num = int(sys.stdin.readline()) # 몇번 째 영화를 이름지을지 받아온다.
+count = 0   # 몇번째 영화인지 적는다.
+six = 666   # 영화 제목에 적힐 종말의 숫자이다
+
 while True:
-    if '666' in str(six_n):
-        cnt += 1
-    if cnt == n:
-        print(six_n)
+    if '666' in str(six):   # six에 종말의 숫자가 들어간다면 count +1을 한다.
+        count += 1
+    if count == Num:    # N번째 영화에 도착했다면 해당 종말의 숫자를 출력하고 반복을 멈춘다.
+        print(six)
         break
-    six_n += 1
+    six += 1
